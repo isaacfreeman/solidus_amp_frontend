6967e1748f369c53bec7f7df827d378e171b4a21
Isaac Freeman <isaac@resolvedigital.co.nz>
2017-01-18 11:27:46 +1300

    AMP home page at /amp
    Rough-and-ready first implementation of AMP. Lots here I intend to tidy up later.
    
    - Adds required AMP header code directly into `spree_application.amp.erb`
    - Adds header links directly into `spree_application.html.erb`
    - Overrides home page views and partials with AMP versions - no other changes at this point
    - Adds AMP MIME type in initializer
    - Forces using AMP if route has /amp

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
+# Base controller for the application
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :force_amp, if: -> { request.path_parameters[:amp] }
+
+  private
+
+  def force_amp
+    request.format = "amp"
+  end
 end
